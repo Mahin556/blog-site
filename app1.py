3d8a3110cda9db3d0313cb25ba76ef46eaea051d
root <root@localhost.localdomain>
2025-06-30 10:13:36 +0530

    add config.json

--- app1.py
+++ app1.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 app = Flask(__name__)
+
+with open("config.json","r") as c:
+    params=json.load(c)["params"]
 
 # import pymysql
 # pymysql.install_as_MySQLdb()
 
 # configure the SQLite database, relative to the app instance folder
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/db_name'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:Pass_word1@localhost/devopsclub'
+
+if params['local_server'] == True:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['local_db_uri']
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['prod_db_uri']
+
 db = SQLAlchemy(app)
 
 class Contacts(db.Model):
@@ -25,11 +34,11 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", params=params)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 @app.route("/contact", methods = ['GET', 'POST'])
 def contact():
@@ -42,13 +51,12 @@ def contact():
         entry = Contacts(name=name, email=email, phone_num=phone, msg=msg, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
-
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 app.run(debug=True,host='192.168.29.173')
 
